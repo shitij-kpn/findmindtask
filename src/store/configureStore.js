1c0dfbaa25905bf92ddb4d56acab7be5ac0c3e6c
shitij_kpn <38817435+shitij-kpn@users.noreply.github.com>
2020-06-15 07:15:13 +0000

    removed devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,11 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import userReducer from "./../reducer/userReducer";
 
 export default () => {
   const store = createStore(
     userReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(thunk)
   );
   return store;
 };
